55ecd7cb8e16a1332e8e621d426c639ce9501c2b
spham64 <samuelpham64@gmail.com>
2025-02-22 18:49:26 -0500

    (2:39:03) Flash Messages & (2:41:45) User Logout

--- django_tutorial/base/views.py
+++ django_tutorial/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -15,16 +17,27 @@ from .forms import RoomForm
 def loginPage(request):
 
     if request.method == "POST":
-        email = request.POST.get('username')
+        username = request.POST.get('username')
         password = request.POST.get('password')
 
         try:
             user = User.objects.get(username=username)
         except:
-            
+            messages.error(request, "User does not exist")
 
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
     context = {}
     return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ""
