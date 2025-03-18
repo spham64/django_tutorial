fa671ca07be56080ec89d6e3fa87e8ffe96f93e1
spham64 <samuelpham64@gmail.com>
2025-03-18 15:54:59 -0400

    (7:10:04) Integrating Custom User Model

--- django_tutorial/django_tutorial/settings.py
+++ django_tutorial/django_tutorial/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "base.apps.BaseConfig",
 ]
 
+AUTH_USER_MODEL = "base.user"
+
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
@@ -125,10 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
+MEDIA_URL = "/images/"
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = BASE_DIR / "static/images"
 
 # STATIC_ROOT = 
 
