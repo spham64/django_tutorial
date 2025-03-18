1cbd5c56404c9b2030cd6144b821106c75dfa014
spham64 <samuelpham64@gmail.com>
2025-03-18 12:45:20 -0400

    (6:25:43) Django REST Framework

--- django_tutorial/django_tutorial/settings.py
+++ django_tutorial/django_tutorial/settings.py
@@ -38,10 +38,15 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     
+    
+
+    "rest_framework",
+    "corsheaders",
     "base.apps.BaseConfig",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -131,3 +136,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
